6907db855aca7dbe9a21f4c77be8c446d90f6a10
Jörg F. Wittenberger <Joerg.Wittenberger@softeyes.net>
2020-06-18 13:47:59 +0200

    fix format strng to avoid access of uninitialized values

--- node/Trace.cpp
+++ node/Trace.cpp
@@ -82,10 +82,11 @@ void Trace::peerConfirmingUnknownPath(void *const tPtr,const uint64_t networkId,
 	char tmp[128];
 	if (!path) return; // sanity check
 
-	ZT_LOCAL_TRACE(tPtr,RR,"trying unknown path %s to %.10llx (packet %.16llx verb %d local socket %lld network %.16llx)",path->address().toString(tmp),peer.address().toInt(),packetId,(double)verb,path->localSocket(),networkId);
+	ZT_LOCAL_TRACE(tPtr,RR,"trying unknown path %s to %.10lx (packet %.16lx verb %u local socket %ld network %.16lx)",path->address().toString(tmp),peer.address().toInt(),packetId,verb,path->localSocket(),networkId);
 
 	std::pair<Address,Trace::Level> byn;
 	if (networkId) { Mutex::Lock l(_byNet_m); _byNet.get(networkId,byn); }
+	else { byn.first = 0; }
 
 	if ((_globalTarget)||(byn.first)) {
 		Dictionary<ZT_MAX_REMOTE_TRACE_SIZE> d;
